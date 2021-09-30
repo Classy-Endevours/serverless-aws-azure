c6dfa62e1b3887e6bfbb55372d518a266db59e53
Manthan Mehta <>
2021-10-01 02:26:43 +0530

    Added a Demo Route and response

---
+++ api/auth.ts
@@ -0,0 +1,13 @@
+import Response from "../util/response";
+
+module.exports.getToken = async () => {
+    try {
+      return Response.create(200, {
+        str:'Hello World'
+      })
+    } catch (error) {
+      return Response.create(500, {
+        err: error.message
+      })
+    }
+  };
