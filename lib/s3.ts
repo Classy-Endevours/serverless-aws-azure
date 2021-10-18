19eafded1ec838e35415e873ed6cb24b2fa7b78e
Pushpak Sharma <sharmatryhard@gmail.com>
2021-10-18 20:13:07 +0530

    v4

--- lib/s3.ts
+++ lib/s3.ts
@@ -8,6 +8,7 @@ const s3 = new AWS.S3();
 
 export const uploadObject = (body) => {
   return new Promise((resolve, reject) => {
+    console.log("process start: ", body, process.env.AWS_ACCESS_KEY_ID)
     s3.putObject(body, function (err, data) {
       if (err) {
         console.log('Some error occurred: ', err)
