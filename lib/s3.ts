c55b1b5d2799836bee8d220c8589a50a0995a709
Manthan Mehta <manthanmehta810@gmail.com>
2021-10-18 23:35:20 +0530

    Put Object access

--- lib/s3.ts
+++ lib/s3.ts
@@ -1,16 +1,16 @@
 import * as AWS from "aws-sdk";
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: "us-east-1",
-});
+// AWS.config.update({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   region: "us-east-1",
+// });
 const s3 = new AWS.S3();
 
 export const uploadObject = async (body) => {
   return new Promise((resolve, reject) => {
     try {  
       console.log("process start: ", body, process.env.AWS_ACCESS_KEY_ID);
-      s3.upload(body, function (err, data) {
+      s3.putObject(body, function (err, data) {
         if (err) {
           console.log("Some error occurred: ", err);
           reject(err);
