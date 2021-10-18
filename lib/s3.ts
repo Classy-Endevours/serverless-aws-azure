60cc20a393e4ede99b1b6d5d1a98d1ffb9035508
Pushpak Sharma <sharmatryhard@gmail.com>
2021-10-18 20:20:50 +0530

    added s3 update

--- lib/s3.ts
+++ lib/s3.ts
@@ -1,9 +1,9 @@
 import * as AWS from "aws-sdk";
-// AWS.config.update({
-//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//   region: "us-east-1",
-// });
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: "us-east-1",
+});
 const s3 = new AWS.S3();
 
 export const uploadObject = (body) => {
