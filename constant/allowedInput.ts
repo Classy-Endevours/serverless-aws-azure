fd6e2e325ff203b3fe4bc473ddf7caade7e68f4a
Pushpak Sharma <sharmatryhard@gmail.com>
2021-10-19 20:27:30 +0530

    Update getting-started.md (#9)
    * Update getting-started.md
    
    * added mime types

--- constant/allowedInput.ts
+++ constant/allowedInput.ts
@@ -1,1 +1,8 @@
-export const ALLOWED_MIME_TYPE = ["image/jpeg", "image/png", "image/jpg"];
+export const ALLOWED_MIME_TYPE = [
+  "image/jpeg",
+  "image/png",
+  "image/jpg",
+  "application/pdf",
+  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+  "application/msword",
+];
