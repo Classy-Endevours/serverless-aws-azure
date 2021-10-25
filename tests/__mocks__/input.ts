fea11c64760be2c07e7b68c71d84773f7a6cb3f6
Pushpak Sharma <sharmatryhard@gmail.com>
2021-10-25 20:23:12 +0530

    increased coverage

---
+++ tests/__mocks__/input.ts
@@ -0,0 +1,8 @@
+export const report = {
+    save: {
+        success: `{"image":"test","mime":"image/jpeg","description":"test"}`,
+        successPartial: `{"description":"test"}`,
+        failed: `{"image":"test","mime":"image/jpeg"}`,
+        failedPartial: `{"image":"test","mime":"test","description":"test"}`,
+    }
+}
