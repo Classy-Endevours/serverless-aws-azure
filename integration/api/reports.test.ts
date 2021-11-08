2129622761b978d73d7fbacb12e56156929e1c42
Pushpak Sharma <sharmatryhard@gmail.com>
2021-11-08 22:42:46 +0530

    Fixed integration testing

--- integration/api/reports.test.ts
+++ integration/api/reports.test.ts
@@ -4,13 +4,16 @@ import fakeData from "../__mocks__/faker";
 
 describe("Integration test for find api", () => {
   beforeEach(async () => {
+    await prisma.statusReports.deleteMany();
     await prisma.reports.deleteMany();
   });
   afterAll(async () => {
+    await prisma.statusReports.deleteMany();
+    await prisma.reports.deleteMany();
     await prisma.$disconnect();
   });
   it("should return 200 with one entry in database for find", async () => {
-    const input = fakeData.reports.oneReport();
+    const input: any = fakeData.reports.oneReport();
     await prisma.reports.create({
       data: input,
     });
@@ -22,7 +25,7 @@ describe("Integration test for find api", () => {
     );
   });
   it("should return 200 with more than one entry in database for find", async () => {
-    const input = fakeData.reports.manyReport(4);
+    const input: any = fakeData.reports.manyReport(4);
     await prisma.reports.createMany({
       data: input,
     });
